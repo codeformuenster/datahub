8fedfe80e4f78b0bf6e23bbcf97713938b4da888
Tobias Bradtke <webwurst@gmail.com>
2016-03-05 02:14:45 +0100

    Try that

--- rancher/elasticsearch-config/run.sh
+++ rancher/elasticsearch-config/run.sh
@@ -14,16 +14,20 @@ if [ -f "$PLUGIN_TXT" ]; then
   done
 fi
 
-set -ex \
-  && cd /usr/share/elasticsearch/config/ \
-  && for path in \
-    ./data \
-    ./logs \
-    ./config \
-    ./config/scripts \
-  ; do \
-    mkdir -p "$path"; \
-    chown -R elasticsearch:elasticsearch "$path"; \
-  done
+# set -ex \
+#   && cd /usr/share/elasticsearch/config/ \
+#   && for path in \
+#     ./data \
+#     ./logs \
+#     ./config \
+#     ./config/scripts \
+#   ; do \
+#     mkdir -p "$path"; \
+#     chown -R elasticsearch:elasticsearch "$path"; \
+#   done
+
+# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/config
+chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
 
 exec /docker-entrypoint.sh elasticsearch
